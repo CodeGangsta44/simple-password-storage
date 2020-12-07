fe73758a44e9a9993abc8da626cbfcc6f9e388d9
Yurii Matora <yura.matora11@gmail.com>
2020-12-07 19:27:22 +0200

    [TECH] Create project structure

---
+++ src/main/java/com/yurwar/simplepasswordstorage/SimplePasswordStorageApplication.java
@@ -0,0 +1,13 @@
+package com.yurwar.simplepasswordstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePasswordStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePasswordStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yurwar/simplepasswordstorage/SimplePasswordStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurwar.simplepasswordstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePasswordStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
